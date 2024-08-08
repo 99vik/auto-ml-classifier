abc496a719e1d3bfe263c146d431c760620c3d6a
Viktor Miric <viktorm47@gmail.com>
2024-08-08 15:34:15 +0200

    added tw prettier plugin for auto class sorting

--- src/app/datasets/page.tsx
+++ src/app/datasets/page.tsx
@@ -1,27 +1,27 @@
-'use client';
+"use client";
 
-import { readFiles, removeFiles } from '@/actions';
-import CsvFileDataDialog from '@/components/CsvFileDataDialog';
-import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
+import { readFiles, removeFiles } from "@/actions";
+import CsvFileDataDialog from "@/components/CsvFileDataDialog";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import UploadDialog from '@/components/UploadDialog';
-import { cn, formatBytes } from '@/lib/utils';
-import { useQuery } from '@tanstack/react-query';
-import { Ellipsis, Trash } from 'lucide-react';
-import { useState } from 'react';
+} from "@/components/ui/dropdown-menu";
+import UploadDialog from "@/components/UploadDialog";
+import { cn, formatBytes } from "@/lib/utils";
+import { useQuery } from "@tanstack/react-query";
+import { Ellipsis, Trash } from "lucide-react";
+import { useState } from "react";
 
 export default function Page() {
   const [checkedBoxes, setCheckedBoxes] = useState<string[]>([]);
   const [allChecked, setAllChecked] = useState(false);
 
   const { data: files, refetch } = useQuery({
-    queryKey: ['files'],
+    queryKey: ["files"],
     queryFn: () => readFiles(),
     refetchOnWindowFocus: false,
   });
@@ -32,18 +32,18 @@ export default function Page() {
     <div className="h-full">
       <div className="flex items-center justify-between">
         <h1 className="text-3xl font-semibold">Datasets</h1>
-        <div className="flex gap-2 items-center">
+        <div className="flex items-center gap-2">
           <UploadDialog refetch={refetch} />
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button
                 variant="outline"
-                className="focus-visible:ring-0 relative"
+                className="relative focus-visible:ring-0"
               >
                 <div
                   className={cn(
-                    'text-secondary text-xs hidden bg-primary size-4 items-center justify-center rounded-full aspect-square absolute -top-1 -right-1',
-                    checkedBoxes.length > 0 && 'flex'
+                    "absolute -right-1 -top-1 hidden aspect-square size-4 items-center justify-center rounded-full bg-primary text-xs text-secondary",
+                    checkedBoxes.length > 0 && "flex",
                   )}
                 >
                   {checkedBoxes.length}
@@ -52,7 +52,7 @@ export default function Page() {
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-              <DropdownMenuItem className="text-destructive  gap-2 hover:text-destructive">
+              <DropdownMenuItem className="gap-2 text-destructive hover:text-destructive">
                 <Trash size={12} />
                 Delete datasets
               </DropdownMenuItem>
@@ -71,18 +71,18 @@ export default function Page() {
           </Button> */}
         </div>
       </div>
-      <div className="h-px w-full bg-foreground/20 my-2" />
+      <div className="my-2 h-px w-full bg-foreground/20" />
       {files === undefined ? (
-        <div className="h-[500px] animate-pulse bg-background rounded-xl border overflow-hidden"></div>
+        <div className="h-[500px] animate-pulse overflow-hidden rounded-xl border bg-background"></div>
       ) : (
         <>
           {files!.length === 0 ? (
-            <div className="flex mt-10 items-center justify-center font-semibold">
+            <div className="mt-10 flex items-center justify-center font-semibold">
               <p>No datasets found.</p>
             </div>
           ) : (
-            <div className="bg-background rounded-xl border overflow-hidden">
-              <div className="grid grid-cols-5 font-semibold bg-zinc-50 p-4 border-b">
+            <div className="overflow-hidden rounded-xl border bg-background">
+              <div className="grid grid-cols-5 border-b bg-zinc-50 p-4 font-semibold">
                 <div className="col-span-2 flex items-center gap-3">
                   <Checkbox
                     checked={
@@ -110,7 +110,7 @@ export default function Page() {
                 {files!.map((file) => (
                   <div
                     key={file.name}
-                    className="grid items-center grid-cols-5 p-4 text-sm"
+                    className="grid grid-cols-5 items-center p-4 text-sm"
                   >
                     <div className="col-span-2 flex items-center gap-3">
                       <Checkbox
@@ -121,7 +121,7 @@ export default function Page() {
                           } else {
                             setAllChecked(false);
                             setCheckedBoxes((prev) =>
-                              prev.filter((item) => item !== file.path)
+                              prev.filter((item) => item !== file.path),
                             );
                           }
                         }}
@@ -130,10 +130,10 @@ export default function Page() {
                     </div>
                     <p className="">{formatBytes(file.sizeInBytes)}</p>
                     <p className="">
-                      {file.importedTime.toLocaleString('en-DE', {
-                        day: 'numeric',
-                        month: 'long',
-                        year: 'numeric',
+                      {file.importedTime.toLocaleString("en-DE", {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
                       })}
                     </p>
                     <div>
