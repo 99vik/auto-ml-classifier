b6e86a0b77f2c2f1c4c9af09e203c34bc84485f7
Viktor Miric <viktorm47@gmail.com>
2024-08-20 17:09:14 +0200

    logic for connecting neurons

--- src/app/train/[fileName]/_components/NeuralNetworkArchitecture.tsx
+++ src/app/train/[fileName]/_components/NeuralNetworkArchitecture.tsx
@@ -23,27 +23,58 @@ export default function NeuralNetworkArchitecture() {
       });
       return positions;
     });
-    console.log(allPositions);
+    // console.log(allPositions);
+    setPositions(allPositions);
+    // console.log(allPositions);
     // console.log(ref1.current.children[0].offsetLeft);
     // console.log(ref1.current.children[0].offsetTop);
     // console.log(ref2.current.children[0].getBoundingClientRect());
   }, []);
 
   function drawLines() {
-    const lines = positions.map((positions, index) => {});
-    return <line x1="276px" y1="127px" x2="75%" y2="25%" stroke="gray" />;
+    console.log(positions);
+    const lines = positions.map((layer, index) => {
+      if (index === positions.length - 1) {
+        return null;
+      }
+      const layerLines = layer.map(
+        (neuron: { x: number; y: number }, index2: number) => {
+          const neuronLines = positions[index + 1].map(
+            (nextNeuron: { x: number; y: number }) => {
+              return (
+                <line
+                  key={`${index}-${index2}`}
+                  x1={neuron.x + 20}
+                  y1={neuron.y + 20}
+                  x2={nextNeuron.x + 20}
+                  y2={nextNeuron.y + 20}
+                  stroke="black"
+                  strokeWidth={1}
+                />
+              );
+            },
+          );
+          return neuronLines;
+        },
+      );
+      return layerLines;
+    });
+    // console.log(positions);
+    return lines;
   }
 
   return (
     <div ref={ref} className="relative flex h-[400px] justify-evenly">
-      <svg className="absolute h-full w-full">{drawLines()}</svg>
+      <svg className="absolute h-full w-full">
+        {positions.length > 0 && drawLines()}
+      </svg>
       <div className="flex flex-col justify-evenly">
-        <div className="size-10 rounded-full bg-blue-500" />
-        <div className="size-10 rounded-full bg-blue-500" />
+        <div className="z-10 size-10 rounded-full bg-primary" />
+        <div className="z-10 size-10 rounded-full bg-primary" />
       </div>
 
       <div className="flex flex-col justify-evenly">
-        <div className="size-10 rounded-full bg-blue-500" />
+        <div className="z-10 size-10 rounded-full bg-primary" />
       </div>
     </div>
   );
