8c2af9d78886d544b5e60c9c9cfc92adc5ada7d7
Viktor Miric <viktorm47@gmail.com>
2024-08-23 14:36:19 +0200

    redisegned confusion matrix layout

--- src/app/train/[fileName]/_components/Charts.tsx
+++ src/app/train/[fileName]/_components/Charts.tsx
@@ -148,59 +148,68 @@ export default function Charts({
             Compare predicted values with the true values.
           </CardDescription>
         </CardHeader>
-        <CardContent className="relative space-y-2 pl-40 pr-52 text-sm font-medium">
-          <p className="absolute right-36 top-1/2 -rotate-90 text-xl text-muted-foreground">
-            Predicted
-          </p>
-
-          <div className={cn("grid", `grid-cols-${labels.length + 1}`)}>
-            {[""].concat(labels).map((label, index) => (
+        <CardContent className="relative flex pb-12 pl-10 pr-16 text-sm font-medium">
+          <div className="grid w-fit grid-cols-1 gap-1 pr-5">
+            <div className="h-8"></div>
+            {labels.map((label, index) => (
               <div
                 key={`${label}-${index}`}
-                className="flex h-10 w-full items-center justify-center"
+                className="flex h-20 items-center justify-center"
               >
                 {label}
               </div>
             ))}
           </div>
-          {labels.map((label, labelIndex) => (
-            <div
-              key={`${labelIndex}-${label}`}
-              className={cn("grid gap-2", `grid-cols-${labels.length + 1}`)}
-            >
-              <div className="flex w-full items-center justify-end pr-5">
-                {label}
-              </div>
-              {data[0]
-                ? data[data.length - 1].confusionMatrix[labelIndex].map(
-                    (value, index) => (
+          <div className="flex-1 space-y-1">
+            <p className="absolute right-0 top-1/2 -translate-y-1/2 -rotate-90 text-xl text-muted-foreground">
+              Predicted
+            </p>
+
+            <div className={cn("grid", `grid-cols-${labels.length}`)}>
+              {labels.map((label, index) => (
+                <div
+                  key={`${label}-${index}`}
+                  className="flex h-8 w-full items-center justify-center"
+                >
+                  {label}
+                </div>
+              ))}
+            </div>
+            {labels.map((label, labelIndex) => (
+              <div
+                key={`${labelIndex}-${label}`}
+                className={cn("grid gap-1", `grid-cols-${labels.length}`)}
+              >
+                {data[0]
+                  ? data[data.length - 1].confusionMatrix[labelIndex].map(
+                      (value, index) => (
+                        <div
+                          key={index}
+                          className={cn(
+                            "flex h-20 w-full items-center justify-center rounded-lg border",
+                            index === labelIndex
+                              ? "bg-green-200"
+                              : "bg-red-200",
+                          )}
+                        >
+                          {value}
+                        </div>
+                      ),
+                    )
+                  : [...Array(labels.length)].map((_, index) => (
                       <div
                         key={index}
                         className={cn(
                           "flex h-20 w-full items-center justify-center rounded-lg border",
-                          index === labelIndex ? "bg-green-200" : "bg-red-200",
+                          index === labelIndex ? "bg-green-100" : "bg-red-100",
                         )}
                       >
-                        {value}
+                        -
                       </div>
-                    ),
-                  )
-                : [...Array(labels.length)].map((_, index) => (
-                    <div
-                      key={index}
-                      className={cn(
-                        "flex h-20 w-full items-center justify-center rounded-lg border",
-                        index === labelIndex ? "bg-green-100" : "bg-red-100",
-                      )}
-                    >
-                      -
-                    </div>
-                  ))}
-            </div>
-          ))}
-          <div className={cn("mt-2 grid", `grid-cols-${labels.length + 1}`)}>
-            <div></div>
-            <p className="col-span-3 text-center text-xl text-muted-foreground">
+                    ))}
+              </div>
+            ))}
+            <p className="absolute bottom-4 left-1/2 -translate-x-1/2 text-center text-xl text-muted-foreground">
               Expected
             </p>
           </div>
@@ -209,3 +218,6 @@ export default function Charts({
     </div>
   );
 }
+
+// necessary classes for grid to render properly every time
+// grid-cols-2 grid-cols-3 grid-cols-4 grid-cols-5 grid-cols-6 grid-cols-7 grid-cols-8 grid-cols-9 grid-cols-10 grid-cols-11 grid-cols-12
