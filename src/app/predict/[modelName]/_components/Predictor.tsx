4cd1f425f1ec7d2d82fcad756a652b81a361f852
Viktor Miric <viktorm47@gmail.com>
2024-08-28 17:18:43 +0200

    displaying prediction result

--- src/app/predict/[modelName]/_components/Predictor.tsx
+++ src/app/predict/[modelName]/_components/Predictor.tsx
@@ -23,6 +23,7 @@ import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 
 export default function Predictor({ modelData }: { modelData: ModelData }) {
+  const [result, setResult] = useState<string | null>(null);
   const [inputData, setInputData] = useState<InputData>(() => {
     const a: InputData = {};
     modelData.columns
@@ -32,20 +33,22 @@ export default function Predictor({ modelData }: { modelData: ModelData }) {
       });
     return a;
   });
-  console.log(inputData);
-  console.log(modelData);
 
-  const {
-    mutate: predict,
-    data,
-    isPending,
-  } = useMutation({
-    mutationFn: () => predictLabel(modelData, inputData),
+  const { mutate: predict } = useMutation({
+    mutationFn: async () => {
+      const result = await predictLabel(modelData, inputData);
+      setResult(
+        modelData.dataByLabels[modelData.labelIndex][
+          modelData.labels.findIndex((label) => label === result)
+        ],
+      );
+    },
   });
 
-  console.log(data);
+  console.log(modelData);
 
   function handleInputChange(column: string, value: string | number) {
+    setResult(null);
     setInputData((prevState: InputData) => {
       return { ...prevState, [column]: Number(value) };
     });
@@ -60,84 +63,93 @@ export default function Predictor({ modelData }: { modelData: ModelData }) {
   }
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Select inputs</CardTitle>
-        <CardDescription>
-          Select inputs for the model to make predictions.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="space-y-4">
-          {modelData.columns
-            .filter(
-              (_: string, index: number) => index !== modelData.labelIndex,
-            )
-            .map((column: string, index: number) => {
-              const columnData = modelData.dataByLabels.filter(
-                (_: any, indexData: number) =>
-                  indexData !== modelData.labelIndex,
-              )[index];
-              const isNumber = columnData === "Number";
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Select inputs</CardTitle>
+          <CardDescription>
+            Select inputs for the model to make predictions.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-4">
+            {modelData.columns
+              .filter(
+                (_: string, index: number) => index !== modelData.labelIndex,
+              )
+              .map((column: string, index: number) => {
+                const columnData = modelData.dataByLabels.filter(
+                  (_: any, indexData: number) =>
+                    indexData !== modelData.labelIndex,
+                )[index];
+                const isNumber = columnData === "Number";
 
-              return (
-                <div
-                  key={index}
-                  className="grid grid-cols-2 items-center gap-4"
-                >
-                  <p>{column}</p>
-                  {isNumber ? (
-                    <Input
-                      id={column}
-                      type="number"
-                      placeholder={`Enter ${column}`}
-                      onWheel={(e: any) => e.target.blur()}
-                      onChange={(e) =>
-                        handleInputChange(column, Number(e.target.value))
-                      }
-                      className="w-full [&::-webkit-inner-spin-button]:appearance-none"
-                    />
-                  ) : (
-                    <Select
-                      onValueChange={(value) =>
-                        handleInputChange(column, value)
-                      }
-                    >
-                      <SelectTrigger className="w-full">
-                        <SelectValue placeholder={`Select ${column}`} />
-                      </SelectTrigger>
-                      <SelectContent>
-                        {columnData.map(
-                          (option: string, columnIndex: number) => (
-                            <SelectItem
-                              key={`${column}-${option}`}
-                              value={String(
-                                columnData.findIndex(
-                                  (value) => value === option,
-                                ),
-                              )}
-                            >
-                              {option}
-                            </SelectItem>
-                          ),
-                        )}
-                      </SelectContent>
-                    </Select>
-                  )}
-                </div>
-              );
-            })}
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button
-          onClick={handlePredict}
-          //   onClick={handlePredict}
-          className="w-full"
-        >
-          Make Prediction
-        </Button>
-      </CardFooter>
-    </Card>
+                return (
+                  <div
+                    key={index}
+                    className="grid grid-cols-2 items-center gap-4"
+                  >
+                    <p>{column}</p>
+                    {isNumber ? (
+                      <Input
+                        id={column}
+                        type="number"
+                        placeholder={`Enter ${column}`}
+                        onWheel={(e: any) => e.target.blur()}
+                        onChange={(e) =>
+                          handleInputChange(column, Number(e.target.value))
+                        }
+                        className="w-full [&::-webkit-inner-spin-button]:appearance-none"
+                      />
+                    ) : (
+                      <Select
+                        onValueChange={(value) =>
+                          handleInputChange(column, value)
+                        }
+                      >
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder={`Select ${column}`} />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {columnData.map(
+                            (option: string, columnIndex: number) => (
+                              <SelectItem
+                                key={`${column}-${option}`}
+                                value={String(
+                                  columnData.findIndex(
+                                    (value) => value === option,
+                                  ),
+                                )}
+                              >
+                                {option}
+                              </SelectItem>
+                            ),
+                          )}
+                        </SelectContent>
+                      </Select>
+                    )}
+                  </div>
+                );
+              })}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button onClick={handlePredict} className="w-full">
+            Make Prediction
+          </Button>
+        </CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Prediction</CardTitle>
+          <CardDescription>The prediction for the input data.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="text-center text-lg font-medium">
+            {result ? result : "-"}
+          </p>
+        </CardContent>
+      </Card>
+    </>
   );
 }
